e04089265ea4824d249d016e0cd457a79683eb49
pardue95 <85760640+pardue95@users.noreply.github.com>
2021-10-23 16:30:04 -0500

    added luxon for current date

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,28 +3,25 @@ var cityInputEl = document.querySelector("#selected-city");
 var citySearchEl = document.querySelector("#current-city")
 var forecastInputEl = document.querySelector("#forecast-container")
 var tempOutputEl = document.querySelector("#temp");
-var windOutput =   document.querySelector("#wind");
+var windOutput = document.querySelector("#wind");
 var humidityOutputEl = document.querySelector("#humidity")
 var uvOutputEl = document.querySelector("#uv-index")
 
-console.log(uvOutputEl)
-console.log(cityFormEl)
-
-
-
+var dateTime = luxon.DateTime.now().toFormat('MMMM dd, yyyy')
+console.log(dateTime)
 
 
 var fiveDayForecast = function(cityName) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=1a32bd1a7ece5ed4c04eaf133d9d2a51";
-    console.log(cityName)
+
     // make a get request to url
     fetch(apiUrl)
         .then(function(response) {
             // request was successful
             if (response.ok) {
-                // console.log(response);
+
                 response.json().then(function(data) {
-                    console.log(data.city.coord);
+
                     displayForecast(data, cityName)
 
                     var lat = data.city.coord.lat
@@ -40,9 +37,10 @@ var fiveDayForecast = function(cityName) {
             alert('Unable to connect to OpenWeather');
         });
 
+}
 
 
-};
+
 
 
 // function for submit
@@ -52,7 +50,7 @@ var citySubmitHandler = function(event) {
     // get value from input element
     var selectedCity = cityInputEl.value.trim();
 
-    console.log(selectedCity)
+
     if (selectedCity) {
         fiveDayForecast(selectedCity);
         cityInputEl.value = "";
@@ -66,47 +64,58 @@ var displayForecast = function(forecast, selectedCity) {
     //     forecastInputEl.textContent = "No forecasts found.";
     //     return;
     // }
-    console.log(forecast);
-    console.log(selectedCity);
+
     // clear old content
     citySearchEl.textContent = "";
-    citySearchEl.textContent = selectedCity;
+    citySearchEl.textContent = "    " + selectedCity + "   (" + dateTime + ")";
     console.log(selectedCity);
-}
+};
+
 cityFormEl.addEventListener("submit", citySubmitHandler);
 
 
 var getUV = function(lat, long) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&exclude=hourly,minutely,alerts&appid=1a32bd1a7ece5ed4c04eaf133d9d2a51"
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&exclude=hourly,minutely,alerts&appid=1a32bd1a7ece5ed4c04eaf133d9d2a51"
     fetch(apiUrl)
         .then(function(response) {
             response.json().then(function(data) {
-                //     displayUvIndex(data)
-                console.log(data.current.uvi)
-        var uvIndex = data.current.uvi
-displayUvIndex(uvIndex)
+                console.log(data)
+                var uvIndex = data.current.uvi
+                displayUvIndex(uvIndex)
+                var humidity = data.current.humidity
+                humidityOutputEl.innerHTML = " " + humidity + "%";
+
+                var windSpeed = data.current.wind_speed
+                windOutput.innerHTML = " " + windSpeed + " MPH";
+
+                var temp = Math.floor(data.current.temp);
+                tempOutputEl.innerHTML = " " + temp + " ℉";
+
+                var city = data
             });
-        });
-}
+        })
 
-var displayUvIndex = function (uvIndex) {
-    console.log(uvIndex)
-   uvOutputEl.textContent = "";
+    var displayUvIndex = function(uvIndex) {
 
-if ( uvIndex <= 2.99) {
-    uvOutputEl.className += "has-background-success";
-    uvOutputEl.innerHTML = " " + uvIndex}
-    else if ( uvIndex >= 3 && uvIndex <= 5.99) {
-        uvOutputEl.className += "has-background-warning";
-        uvOutputEl.innerHTML = " " + uvIndex
-    } else if ( uvIndex >= 6 && uvIndex <= 7.99) {
-uvOutputEl.style.backgroundColor = "orange";
-        uvOutputEl.innerHTML = " " + uvIndex
-    } else if (uvIndex >= 8 && uvIndex <= 10.99) {
-        uvOutputEl.className += "has-background-danger";
-        uvOutputEl.innerHTML = " " + uvIndex 
-    } else {
-        uvOutputEl.style.backgroundColor = "#EE82EE";
-        uvOutputEl.innerHTML = " " + uvIndex
+console.log(uvIndex)
+
+        if (uvIndex <= 2.99) {
+            uvOutputEl.className += "has-background-success";
+            
+        } else if (uvIndex >= 3 && uvIndex <= 5.99) {
+            uvOutputEl.className += "has-background-warning";
+            
+        } else if (uvIndex >= 6 && uvIndex <= 7.99) {
+            uvOutputEl.style.backgroundColor = "orange";
+            
+        } else if (uvIndex >= 8 && uvIndex <= 10.99) {
+            uvOutputEl.className += "has-background-danger";
+            
+        } else {
+            uvOutputEl.style.backgroundColor = "#EE82EE";
+            
+        }
+         uvOutputEl.innerHTML = " " + uvIndex
     }
-}
+   
+};
