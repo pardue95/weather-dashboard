e68c77758d61ee8c5fc0cd4cae07840de41e52dd
pardue95 <85760640+pardue95@users.noreply.github.com>
2021-10-23 14:56:52 -0500

    add api for lat long and UVINDEX

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,20 @@ var cityFormEl = document.querySelector("#city-form")
 var cityInputEl = document.querySelector("#selected-city");
 var citySearchEl = document.querySelector("#current-city")
 var forecastInputEl = document.querySelector("#forecast-container")
+var tempOutputEl = document.querySelector("#temp");
+var windOutput =   document.querySelector("#wind");
+var humidityOutputEl = document.querySelector("#humidity")
+var uvOutputEl = document.querySelector("#uv-index")
+
+console.log(uvOutputEl)
+console.log(cityFormEl)
+
+
+
 
 
 var fiveDayForecast = function(cityName) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=1a32bd1a7ece5ed4c04eaf133d9d2a51";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=1a32bd1a7ece5ed4c04eaf133d9d2a51";
     console.log(cityName)
     // make a get request to url
     fetch(apiUrl)
@@ -14,8 +24,13 @@ var fiveDayForecast = function(cityName) {
             if (response.ok) {
                 // console.log(response);
                 response.json().then(function(data) {
-                    console.log(data);
+                    console.log(data.city.coord);
                     displayForecast(data, cityName)
+
+                    var lat = data.city.coord.lat
+                    var long = data.city.coord.lon
+                    getUV(lat, long)
+
                 });
             } else {
                 alert('Error: ' + response.statusText);
@@ -24,11 +39,12 @@ var fiveDayForecast = function(cityName) {
         .catch(function(error) {
             alert('Unable to connect to OpenWeather');
         });
-    if (cityName) {
 
-    }
+
 
 };
+
+
 // function for submit
 var citySubmitHandler = function(event) {
     event.preventDefault();
@@ -55,10 +71,42 @@ var displayForecast = function(forecast, selectedCity) {
     // clear old content
     citySearchEl.textContent = "";
     citySearchEl.textContent = selectedCity;
-    console(selectedCity)
+    console.log(selectedCity);
 }
-cityFormEl.addEventListener("submit", citySubmitHandler)
+cityFormEl.addEventListener("submit", citySubmitHandler);
 
 
+var getUV = function(lat, long) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&exclude=hourly,minutely,alerts&appid=1a32bd1a7ece5ed4c04eaf133d9d2a51"
+    fetch(apiUrl)
+        .then(function(response) {
+            response.json().then(function(data) {
+                //     displayUvIndex(data)
+                console.log(data.current.uvi)
+        var uvIndex = data.current.uvi
+displayUvIndex(uvIndex)
+            });
+        });
+}
 
+var displayUvIndex = function (uvIndex) {
+    console.log(uvIndex)
+   uvOutputEl.textContent = "";
 
+if ( uvIndex <= 2.99) {
+    uvOutputEl.className += "has-background-success";
+    uvOutputEl.innerHTML = " " + uvIndex}
+    else if ( uvIndex >= 3 && uvIndex <= 5.99) {
+        uvOutputEl.className += "has-background-warning";
+        uvOutputEl.innerHTML = " " + uvIndex
+    } else if ( uvIndex >= 6 && uvIndex <= 7.99) {
+uvOutputEl.style.backgroundColor = "orange";
+        uvOutputEl.innerHTML = " " + uvIndex
+    } else if (uvIndex >= 8 && uvIndex <= 10.99) {
+        uvOutputEl.className += "has-background-danger";
+        uvOutputEl.innerHTML = " " + uvIndex 
+    } else {
+        uvOutputEl.style.backgroundColor = "#EE82EE";
+        uvOutputEl.innerHTML = " " + uvIndex
+    }
+}
